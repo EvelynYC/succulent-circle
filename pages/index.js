ca96cc2efc5254d111fe24f2af6dd00e6025ba00
Aislyn <aislyn.chuang@wemoscooter.com>
2021-08-15 16:10:09 +0800

    Improve SEO

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Header from '../components/Header';
 import Background from '../components/Background';
 import Footer from '../components/Footer';
@@ -10,17 +11,29 @@ import Articles from '../sections/Articles';
 
 export default function Home() {
   return (
-    <div className={styles.container} id="home">
-      <Background />
-      <Header />
-      <div className={styles.content}>
-        <About />
-        <Portfolio />
-        <Skills />
-        <Articles />
-        <Contact />
+    <>
+      <Head>
+        <title>Hi! I am Aslyn</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="I am Aslyn. I am a front-end web engineer. 我是莊語純，是一個網頁前端工程師。"
+        ></meta>
+        <meta name="keywords" content="Aslyn,莊語純,Aislyn,語純"></meta>
+        <meta httpEquiv="Content-Language" content="en,zh-TW"></meta>
+      </Head>
+      <div className={styles.container} id="home">
+        <Background />
+        <Header />
+        <div className={styles.content}>
+          <About />
+          <Portfolio />
+          <Skills />
+          <Articles />
+          <Contact />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </>
   );
 }
