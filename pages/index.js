fd11a53c71dc5de3cd131ff575ecec0f388add11
Aislyn <aislyn.chuang@wemoscooter.com>
2021-08-18 22:23:32 +0800

    Add GA

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import Head from 'next/head';
-import Header from '../components/Header';
-import Background from '../components/Background';
-import Footer from '../components/Footer';
-import About from '../sections/About';
-import Skills from '../sections/Skills';
-import Portfolio from '../sections/Portfolio';
-import Contact from '../sections/Contact';
+import Header from '@components/Header';
+import Background from '@components/Background';
+import Footer from '@components/Footer';
+import About from '@sections/About';
+import Skills from '@sections/Skills';
+import Portfolio from '@sections/Portfolio';
+import Contact from '@sections/Contact';
+import Articles from '@sections/Articles';
+import LangSwitch from '@components/LangSwitch';
 import styles from '../styles/home.module.scss';
-import Articles from '../sections/Articles';
 
 export default function Home() {
   return (
@@ -22,6 +23,18 @@ export default function Home() {
         <meta name="keywords" content="Aslyn,莊語純,Aislyn,語純" />
         <meta httpEquiv="Content-Language" content="en,zh-TW" />
         <meta charset="UTF-8" />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-KZB880WLEF"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: ` window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+                      gtag('config', 'G-KZB880WLEF');`,
+          }}
+        />
       </Head>
       <div className={styles.container} id="home">
         <Background />
